aa43e3c65cb1372f515341f8397a53511641e594
MM13 <Fastsoccer13@gmail.com>
2019-10-19 23:29:10 -0400

    man...

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,71 @@
+
+    let trivaQnA = [
+    {
+        question: "Who did All Might choose to pass his power to?",
+        answers: ["Izuku Midoriya", "Shoto Todoroki", " Katsuki Bakugo", "Tenya Iida"],
+    },
+
+    {
+        question: "Which student uses fire and ice as their power?",
+        answers: ["Mashirao Ojiro", "Ochaco Uraraka", "Denki Kaminari", "Shoto Todoroki"],
+    },
+
+    {
+        question: "What is Izuku Midoriyas hero name?",
+        answers: ["Goku", "Deku", "One Punch Man", "Kirito"],
+    },
+
+    {
+        question: "What is the name of the High School the students attend?",
+        answers: ["Boone", "UA", "Hero's", "Wawa"],
+    },
+
+    {
+        question: "How long did Izuku Midoriya train to gain his power?",
+        answers: ["12 months", "9 months", "6 months", "3 months"],
+    },
+
+    {
+        question: "Who won the Battle Tournamen in the Sports Festival?",
+        answers: ["Shoto Todoroki", "Katsuki Bakugo", "Eijiro Kirishima", "Mezo Shoji"],
+    },
+
+    {
+        question: "Who hospitalized Tenya Iidas older brother?",
+        answers: ["Mario", "The Hero Killer", "Jason", "FIJI"],
+    },
+];
+
+
+document.getElementById("starter").addEventListener("click", countDown(25));
+function countDown(seconds, display) {
+    var display = document.getElementById("display-time");
+    display.innerHTML = +seconds+" seconds";
+    if (seconds < 1){
+        console.log("times up");
+        clearTimeout(timer);
+    }
+    seconds--;
+    var timer = setTimeout('countDown('+seconds+', "'+display+'")', 1000);
+};
+
+// function countDown(seconds,"display-time") {
+//     setTimeout(timeDisplayed, 1000 * 1)
+///    setTimeout(timeDisplayed2, 1000 * 2)
+// }
+
+// }
+// function timeDisplayed() {
+//     document.getElementById("display-time").innerHTML = "25 seconds";
+// }
+// function timeDisplayed2(){
+//     document.getElementById("display-time").innerHTML = "24 seconds";
+// }
+
+
+//document.getElementById("starter").addEventListener("click", countDown(25));
+
+
+    
+
+// console.log(countDown);
